53178130d61dee0e57a34bcc3f3a504d8ff0c7d8
何伟东 <heweidong@oasgames.com>
2019-12-06 15:13:34 +0800

    集成pod

---
+++ Pods/Target Support Files/Pods-LZMobileKitDemo-LZMobileKitDemoUITests/Pods-LZMobileKitDemo-LZMobileKitDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LZMobileKitDemo_LZMobileKitDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LZMobileKitDemo_LZMobileKitDemoUITests
+@end
---
+++ Pods/Target Support Files/Pods-LZMobileKitDemo-LZMobileKitDemoUITests/Pods-LZMobileKitDemo-LZMobileKitDemoUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_LZMobileKitDemo_LZMobileKitDemoUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LZMobileKitDemo_LZMobileKitDemoUITestsVersionString[];
+
